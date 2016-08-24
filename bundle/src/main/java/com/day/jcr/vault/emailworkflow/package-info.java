028e7767fd4be83f969f496e4b7452a235ff34b8
unknown <aaysriva@aaysriva-W7.corp.adobe.com>
2016-08-24 19:34:32 +0530

    sample workflow step that sends mail

---
+++ bundle/src/main/java/com/day/jcr/vault/emailworkflow/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.day.jcr.vault.emailworkflow;
+
+import aQute.bnd.annotation.Version;
---
+++ bundle/src/test/java/com/day/jcr/vault/emailworkflow/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.day.jcr.vault.emailworkflow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
